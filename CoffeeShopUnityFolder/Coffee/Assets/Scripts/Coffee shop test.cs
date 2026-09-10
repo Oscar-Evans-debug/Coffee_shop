04062515d7b985b065558a159b3ca0ae1c322711
Oscar-Evans-debug <evansoscar64@gmail.com>
2026-09-10 16:52:11 +0100

    feat:Food ordering
    Customers will now decide whether they want food or not and if they do they will request a random food from an array of 10

--- CoffeeShopUnityFolder/Coffee/Assets/Scripts/Coffee shop test.cs
+++ CoffeeShopUnityFolder/Coffee/Assets/Scripts/Coffee shop test.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class CoffeeShopTest : MonoBehaviour
 {
+    public int food = 0;
+    public int foodnum = 0;
     public int coffeenum = 0;
     public int coffeesSold = 5;
     public float coffeePrice = 3.50f;
@@ -18,12 +20,22 @@ public class CoffeeShopTest : MonoBehaviour
         CustomerFront();
     }
 
-    // Update is called once per frame
+    //customerFront simulates a customer ordering a random type of coffee from the coffee shop. It creates an array of coffee types, generates a random index to select one of the coffee types, and then logs a message indicating the customer's order.
     void CustomerFront()
     {
+        food=Random.Range(0, 2);
         string[] coffeeType = {"Espresso", "Cappuccino", "Latte", "Americano", "Mocha", "Macchiato", "Affogato", "Cold Brew", "Iced Coffee", "Frappuccino"};
         int coffeenum = Random.Range(0, 10);
-
+        string[] foodtype = {"Croissant", "Muffin", "Bagel", "Scone", "Danish", "Cookie", "Brownie", "Cupcake", "Donut", "Toast"};
+        int foodnum = Random.Range(0, 10);
         Debug.Log("Hello, I would like to buy a " + coffeeType[coffeenum]);
+        if (food == 1)
+        {
+            Debug.Log("I would also like to buy a " + foodtype[foodnum]);
+        }
+    else
+        {
+            Debug.Log("I would not like to buy any food.");
+        }
     }
 }
